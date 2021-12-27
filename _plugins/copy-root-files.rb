4ee622f7f9bab6937bc6a10cc0edbfe59d9c4204
Andrej Stieben <e-mail@stieben.de>
2021-12-27 22:43:14 +0100

    Added logo, updated favicon, plus some minor fixes

---
+++ _plugins/copy-root-files.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+# Author of this script: PAT-O-MATION (https://stackoverflow.com/a/57495308/2040520)
+
+require 'fileutils'
+
+module Jekyll
+  Jekyll::Hooks.register :site, :post_write do |post|
+    Dir.glob('_root/*.*') do |file|
+      sourcePath = file
+      outputPath = File.join('_site', File.basename(file) )
+      FileUtils.cp(sourcePath, outputPath)
+    end
+  end
+end
