3abafb92ee467fd848e088e315f9863e30b3901c
Andrej Stieben <e-mail@stieben.de>
2022-01-02 13:38:52 +0100

    Critical bugfix: CNAME
    Ruby copy plugin ignored files without file extension. This led to the site being offline. Not anymore.

--- _plugins/copy-root-files.rb
+++ _plugins/copy-root-files.rb
@@ -6,7 +6,7 @@ require 'fileutils'
 
 module Jekyll
   Jekyll::Hooks.register :site, :post_write do |post|
-    Dir.glob('_root/*.*') do |file|
+    Dir.glob('_root/*') do |file|
       sourcePath = file
       outputPath = File.join('_site', File.basename(file) )
       FileUtils.cp(sourcePath, outputPath)
